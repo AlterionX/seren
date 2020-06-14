f7ef24b824282ade1c43d26621151352e9c2bb8c
Benjamin Xu <ben.xu.cs@gmail.com>
2020-06-14 00:35:36 -0500

    Add colors
    colors!

--- src/logger.rs
+++ src/logger.rs
@@ -1,44 +1,68 @@
+use chrono::Local;
+use fern::{
+    colors::{Color, ColoredLevelConfig},
+    log_file, Dispatch, InitError,
+};
+use log::LevelFilter;
+
 struct Cfg {
-    level: log::LevelFilter,
+    level: LevelFilter,
     bypass_stdio: bool,
 }
 
 impl Cfg {
-    fn setup_logger(self) -> Result<(), fern::InitError> {
-        let dispatch = fern::Dispatch::new()
-            .format(|out, message, record| {
+    fn setup_logger(self) -> Result<(), InitError> {
+        let file_out = Dispatch::new()
+            .format(|out, message, record|
                 out.finish(format_args!(
                     "{}[{}][{}] {}",
-                    chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                    Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
                     record.target(),
                     record.level(),
-                    message
+                    message,
                 ))
-            })
+            )
+            .chain(log_file("output.log")?);
+        let dispatch = Dispatch::new()
             .level(self.level)
-            .chain(fern::log_file("output.log")?);
+            .chain(file_out);
         let dispatch = if self.bypass_stdio {
             dispatch
         } else {
-            dispatch.chain(std::io::stdout())
+            let colors = ColoredLevelConfig::new()
+                .trace(Color::BrightBlack)
+                .debug(Color::White)
+                .info(Color::BrightWhite)
+                .warn(Color::Yellow)
+                .error(Color::Red);
+            let stdout = Dispatch::new()
+                .format(move |out, message, record| out.finish(format_args!(
+                    "{}[{}][{}] {}",
+                    Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                    record.target(),
+                    colors.color(record.level()),
+                    message,
+                )))
+            .chain(std::io::stdout());
+            dispatch.chain(stdout)
         };
         dispatch.apply().map_err(Into::into)
     }
 }
 
 #[cfg(debug_assertions)]
-pub fn setup() -> Result<(), fern::InitError> {
+pub fn setup() -> Result<(), InitError> {
     Cfg {
-        level: log::LevelFilter::Debug,
+        level: LevelFilter::Debug,
         bypass_stdio: false,
     }
     .setup_logger()
 }
 
 #[cfg(not(debug_assertions))]
-pub fn setup() -> Result<(), fern::InitError> {
+pub fn setup() -> Result<(), InitError> {
     Cfg {
-        level: log::LevelFilter::Info,
+        level: LevelFilter::Info,
         bypass_stdio: true,
     }
     .setup_logger()
