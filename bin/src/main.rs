4bbf2089b070ce19732e18f0c91f68124a846fb1
Benjamin Xu <ben.xu.cs@gmail.com>
2020-10-23 05:48:25 -0500

    Move seren module's submodules (editor, game) up and integrate with the lib package.

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -3,44 +3,44 @@ use structopt::StructOpt;
 use tap::*;
 
 mod game;
-mod seren;
+mod editor;
 
 mod logger;
 mod util;
 mod opts;
 
-fn run_app<State: game::State>(
+fn run_app<State: sl::exec::Sim>(
     mut state: State,
     // Normalized input device
-    mut input: impl game::input::Input<State::ActionEnum>,
+    mut input: impl sl::uial::input::Input<State::ActionEnum>,
     // Normalized output device
-    mut display: impl game::display::Display<State, State::Cfg>,
+    mut display: impl sl::uial::display::Display<State, State::Cfg>,
     // Cfg
     cfg: State::Cfg,
-) -> game::Result<()> {
+) -> sl::SeRes<()> {
     // Render once to get the ball rolling.
     display.display(&state, &cfg)?;
     loop {
         let action = input.next_action()?;
         log::debug!("Executing action {:?}", action);
         match action {
-            game::input::SystemAction::Exit => {
+            sl::uial::input::SystemAction::Exit => {
                 log::info!("System exit command received. Shutting down.");
                 break;
             }
-            game::input::SystemAction::Action(a) => match state.resolve(&cfg, a)? {
-                game::display::RenderMode::Render => display.display(&state, &cfg)?,
-                game::display::RenderMode::Ignore => (),
+            sl::uial::input::SystemAction::Action(a) => match state.resolve(&cfg, a)? {
+                sl::uial::display::RenderMode::Render => display.display(&state, &cfg)?,
+                sl::uial::display::RenderMode::Ignore => (),
             },
         };
     }
     Ok(())
 }
 
-fn main() -> game::Result<()> {
+fn main() -> sl::SeRes<()> {
     logger::setup()
         .tap_err(|e| println!("Fern logger failed to initialize due to {:?}.", e))
-        .map_err(|_| game::Resolution("Fern logger failed to initialize.".to_string()))?;
+        .map_err(|_| sl::exec::ResolutionErr("Fern logger failed to initialize.".to_string()))?;
 
     log::info!("SeRen loading cmdline options.");
     let opts = opts::CommandLineInterface::from_args();
@@ -50,48 +50,48 @@ fn main() -> game::Result<()> {
         "SeRen loading game cfg from {}.",
         opts.game_cfg_path.display()
     );
-    let cfg = seren::GameCfg::load_from(opts.game_cfg_path.as_path())
+    let cfg = game::Cfg::load_from(opts.game_cfg_path.as_path())
         .tap_err(|e| log::error!("Cfg failed to load due to {:?}. Shutting down.", e))?;
     log::debug!("SeRen loaded game cfg {:?}.", cfg);
 
     let res = if opts.use_editor {
         log::info!("Launching SeRen in editor mode.");
         if opts.use_raw_mode {
-            let input = game::input::cmd_line(seren::editor::Action::parse_input);
-            let display = game::display::cmd_line();
+            let input = sl::uial::input::cmd_line(editor::Action::parse_input);
+            let display = sl::uial::display::cmd_line();
             log::trace!("Input and display intialized. Running editor now.");
             run_app(
-                seren::EditorState::new(cfg),
+                editor::State::new(cfg),
                 input,
                 display,
-                seren::EditorCfg,
+                editor::Cfg,
             )
             .tap_err(|e| log::error!("Editor has crashed due to {:?}.", e))
         } else {
-            let input = game::input::cmd_line(seren::editor::Action::parse_input);
-            let display = game::display::raw_cmd_line();
+            let input = sl::uial::input::cmd_line(editor::Action::parse_input);
+            let display = sl::uial::display::raw_cmd_line();
             log::trace!("Input and display intialized. Running editor now.");
             run_app(
-                seren::EditorState::new(cfg),
+                editor::State::new(cfg),
                 input,
                 display,
-                seren::EditorCfg,
+                editor::Cfg,
             )
             .tap_err(|e| log::error!("Editor has crashed due to {:?}.", e))
         }
     } else {
         log::info!("Launching SeRen in game mode.");
         if opts.use_raw_mode {
-            let input = game::input::cmd_line(seren::game::Action::parse_input);
-            let display = game::display::raw_cmd_line();
+            let input = sl::uial::input::cmd_line(game::Action::parse_input);
+            let display = sl::uial::display::raw_cmd_line();
             log::trace!("Input and display intialized. Running game now.");
-            run_app(seren::GameState::init(&cfg)?, input, display, cfg)
+            run_app(game::State::init(&cfg)?, input, display, cfg)
                 .tap_err(|e| log::error!("Game has crashed due to {:?}.", e))
         } else {
-            let input = game::input::cmd_line(seren::game::Action::parse_input);
-            let display = game::display::cmd_line();
+            let input = sl::uial::input::cmd_line(game::Action::parse_input);
+            let display = sl::uial::display::cmd_line();
             log::trace!("Input and display intialized. Running game now.");
-            run_app(seren::GameState::init(&cfg)?, input, display, cfg)
+            run_app(game::State::init(&cfg)?, input, display, cfg)
                 .tap_err(|e| log::error!("Game has crashed due to {:?}.", e))
         }
     };
--- bin/src/seren.rs
+++
@@ -1,5 +0,0 @@
-pub mod editor;
-pub mod game;
-
-pub use self::editor::{Cfg as EditorCfg, State as EditorState};
-pub use self::game::{Cfg as GameCfg, State as GameState};
