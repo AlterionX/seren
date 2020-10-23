1527aa86cccf6c069c4ed9ffdbeedfb23f05e991
Benjamin Xu <ben.xu.cs@gmail.com>
2020-10-23 05:40:33 -0500

    Remove more general implementations.

--- bin/src/seren.rs
+++ bin/src/seren.rs
@@ -1,9 +1,5 @@
-mod lib;
-
 pub mod editor;
 pub mod game;
-pub mod opts;
 
 pub use self::editor::{Cfg as EditorCfg, State as EditorState};
 pub use self::game::{Cfg as GameCfg, State as GameState};
-pub use self::opts::CommandLineInterface;
